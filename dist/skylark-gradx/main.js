6a3b1d0c46045972d0eb31b7c4d2c40f4629f976
hudaokeji <github@hudaokeji.com>
2020-08-06 11:26:19 +0900

    update

---
+++ dist/skylark-gradx/main.js
@@ -0,0 +1,9 @@
+/**
+ * skylark-gradx - The skylark gradient picker widget
+ * @author Hudaokeji, Inc.
+ * @version v0.9.0
+ * @link https://github.com/skylark-integration/skylark-gradx/
+ * @license MIT
+ */
+define(["./Gradienter"],function(e){return e});
+//# sourceMappingURL=sourcemaps/main.js.map
